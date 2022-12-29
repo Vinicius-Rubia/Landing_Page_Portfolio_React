03da24c7f44b7e804a910a8ae1d5e1fb9c2f5b46
vinicius-rubia <vinicius.rubia@negocieonline.com.br>
2022-12-29 19:56:04 -0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <div className="App">
-      <h1>Landing Page</h1>
+      <Header />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import './index.scss';
+
+const Header = () => {
+  return (
+    <header id='header'>
+      <span>SaulDesign</span>
+      <ul>
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Contact</li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header
